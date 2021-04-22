bfe576edfde23bba5b560e53bfc88d92760e25bf
MACDOUGLAS\Thomas <tholebris@gmail.com>
2021-04-22 11:11:24 +0200

    models fait, faire les routes

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+const Product = require('../models/product.model');
+
 //? Créer un produit
 router.post('/createProduct', async (req,res)=>{
+    const product = new Product({
+        productName: req.body.productName,
+        productStock: req.body.productStock,
+        productDescription: req.body.productDescription,
+        productPrice: req.body.productPrice,
+    })
+
+    const result = await product.save();
     
 })
 
